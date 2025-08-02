14843ca617841694b7360c146d225aabb45ae67d
LeoBock18 <leonardo.bocchieri@mail.polimi.it>
2025-08-02 18:35:19 +0200

    adding early stopping and scheduler

--- training/__init__.py
+++ training/__init__.py
@@ -1,10 +1,12 @@
 from .train import train_step, evaluation_step
 from .hypernet_utils import build_state_from_parameters, assign_parameters, apply
+from .early_stopping import EarlyStopping
 
 __all__ = [
     'train_step',
     'evaluation_step',
     'build_state_from_parameters',
     'assign_parameters',
-    'apply'
+    'apply',
+    'EarlyStopping'
 ]
