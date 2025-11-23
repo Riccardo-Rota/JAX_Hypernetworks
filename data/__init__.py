ea4944e6faa121c9c0beb7a9f264aa74f6571d31
LeoBock18 <leonardo.bocchieri@mail.polimi.it>
2025-11-23 14:13:09 +0100

    Removing deprecated iterable dataset

--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,4 @@
 from .dataset import Dataset, JaxDataLoader
-from .iterable_dataset import IterableDataset, IterableJaxDataLoader
 from .create_dataset_turbulence import create_dataset_turbulence, split_dataset_turbulence
 
-__all__ = ['Dataset', 'JaxDataLoader', 'IterableDataset', 'IterableJaxDataLoader', 'create_dataset_turbulence', 'split_dataset_turbulence']
+__all__ = ['Dataset', 'JaxDataLoader', 'create_dataset_turbulence', 'split_dataset_turbulence']
