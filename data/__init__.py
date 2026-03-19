f6ec737a2a4528ddea13fc9cc55ce50e44a3e631
LeoBock18 <leonardo.bocchieri@mail.polimi.it>
2026-03-20 00:23:43 +0100

    Modified init file for grain dataset

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,4 @@
-from .grain_dataset import get_train_pipeline
+from .grain_dataset import InMemoryHDF5Source, ArrayRecordSource, ToyDataSource, get_pipeline
 from .preprocessing import prepare_datasets
 
-__all__ = ['get_train_pipeline', 'prepare_datasets']
+__all__ = ['InMemoryHDF5Source', 'ArrayRecordSource', 'ToyDataSource', 'get_pipeline', 'prepare_datasets']
